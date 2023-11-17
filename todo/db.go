f544bafeea56eb8a65ceb80d9f1fd70dfebcc030
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-11-17 16:13:55 +0700

    fix:db.go error connection

--- todo/db.go
+++ todo/db.go
@@ -2,8 +2,6 @@ package todo
 
 import (
 	"database/sql"
-	"errors"
-	"os"
 
 	"github.com/rs/zerolog/log"
 )
@@ -12,21 +10,37 @@ var (
 	db *sql.DB
 )
 
-func SetDatabase(db_name string) error {
+func SetDatabase() error {
 	var err error
 
-	if db_name == "" {
-		return errors.New("there is no database")
-	}
-
-	// remove old sqlite db if exist
-	os.Remove(db_name)
+	var db_name string = "../todo.db"
 
 	db, err = sql.Open("sqlite3", db_name)
 	if err != nil {
 		log.Error().Err(err).Msg("unable to connect to database")
 	}
-	defer db.Close()
+
+	log.Info().Any("db", db)
+	query := `CREATE TABLE IF NOT EXISTS todolist (
+		id TEXT PRIMARY KEY,
+		title TEXT NOT NULL,
+		created_at DATETIME NOT NULL,
+		done_at DATETIME,
+		is_done BOOLEAN GENERATED ALWAYS AS (done_at IS NOT NULL) STORED
+	);`
+
+	_, err = db.Exec(query)
+
+	log.Info().Any("db", db)
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create database")
+		return err
+	}
 
 	return nil
+}
+
+func getDatabase() *sql.DB {
+	return db
 }
