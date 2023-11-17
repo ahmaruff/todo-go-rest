e1d7d74d9d8b94af592ffc6e4c8ba080a07128ce
Ahmad Maruf <ahmadmaruf2701@gmail.com>
2023-11-17 18:05:16 +0700

    feat:router, kurang delete item

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"net/http"
 	"todo-go/todo"
 
+	"github.com/labstack/echo/v4"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	db_name := "./todo.db"
-	todo.SetDatabase(db_name)
+	todo.SetDatabase()
+
+	e := echo.New()
+	todo.InitTodoRoutes(e)
+
+	log.Info().Msg("Starting up server...")
+
+	if err := e.Start(":8080"); err != http.ErrServerClosed {
+		log.Fatal().Err(err).Msg("failed to start server")
+	}
 }
---
+++ todo/routers.go
@@ -0,0 +1,119 @@
+package todo
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/oklog/ulid/v2"
+)
+
+func InitTodoRoutes(e *echo.Echo) {
+	e.GET("/", listItemHandler)
+	e.POST("/", createItemHandler)
+	e.GET("/:id", getItemHandler)
+	e.POST("/done", makeItemDoneHandler)
+}
+
+func listItemHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+	resp, err := listItems(ctx)
+
+	if err != nil {
+		return echo.NewHTTPError(echo.ErrInternalServerError.Code, err)
+	}
+
+	return c.JSON(200, resp)
+}
+
+func getItemHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	item_id := c.Param("id")
+
+	id, err := ulid.Parse(item_id)
+
+	if err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	var resp TodoItem
+
+	item, err := findItem(ctx, id)
+
+	if err != nil {
+		if err == ErrTodoNotFound {
+			notfound := map[string]string{
+				"message": "item not found",
+			}
+
+			return c.JSON(echo.ErrNotFound.Code, notfound)
+		}
+
+		return echo.NewHTTPError(echo.ErrInternalServerError.Code, err)
+	}
+
+	resp = item
+
+	return c.JSON(200, resp)
+}
+
+func createItemHandler(c echo.Context) error {
+	if err := c.Request().ParseForm(); err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	ctx := c.Request().Context()
+
+	type titleReq struct {
+		Title string `json:"title"`
+	}
+	title := new(titleReq)
+
+	if err := c.Bind(title); err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	id, err := createItem(ctx, title.Title)
+
+	if err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	var resp struct {
+		Id ulid.ULID `json:"id"`
+	}
+
+	resp.Id = id
+
+	return c.JSON(201, resp)
+}
+
+func makeItemDoneHandler(c echo.Context) error {
+	ctx := c.Request().Context()
+
+	type doneReq struct {
+		Id string `json:"id"`
+	}
+
+	done_req := new(doneReq)
+
+	if err := c.Bind(done_req); err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	Id, err := ulid.Parse(done_req.Id)
+
+	if err != nil {
+		return echo.NewHTTPError(echo.ErrBadRequest.Code, err)
+	}
+
+	err = makeItemDone(ctx, Id)
+
+	if err != nil {
+		return echo.NewHTTPError(echo.ErrInternalServerError.Code, err)
+	}
+
+	resp := map[string]string{
+		"message": "item updated",
+	}
+
+	return c.JSON(200, resp)
+}
